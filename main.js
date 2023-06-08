fa9d1495425d4ba9d69198b5758cc9a63f3d5229
P7538 <mixu.souri@gmail.com>
2023-06-08 22:27:07 +0200

    feat: bundle

---
+++ main.js
@@ -0,0 +1,7 @@
+var http = require('http');
+const keepAliveAgent = new http.Agent({ keepAlive: true });
+var ws= require('ws')
+console.log(keepAliveAgent)
+console.log(ws)
+
+//browserify main.js -o bundle.js
